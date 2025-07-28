4944812e0c36cbcb646782f8b7ac6f7aa1f69279
maruchanazalia <maruchanycamaron26@gmail.com>
2025-07-27 20:56:15 -0600

    v.2.2

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from app.routes import router
+import os
 
 app = FastAPI()
 app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
